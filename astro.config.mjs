325045ca4ad1a8a852fb14f6163fa10cacf88ec9
Jeancarlo Javier <naphisto43@gmail.com>
2024-08-07 19:35:43 -0500

    feat: My Skills Secition & prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lexingtonthemes.com/",
-  integrations: [tailwind(), sitemap()]
+  site: 'https://lexingtonthemes.com/',
+  integrations: [tailwind(), sitemap()],
 });
